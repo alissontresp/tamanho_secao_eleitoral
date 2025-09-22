bcd8490ac1bfded2f7a0bce2ddb2411bd46463c9
olympiotneto <olympiotneto@gmail.com>
2025-09-22 18:14:13 -0300

    Curva ROC

--- R/modelo_calibracao.R
+++ R/modelo_calibracao.R
@@ -17,8 +17,8 @@ dados <- readr::read_csv2(
   janitor::clean_names() |>
   dplyr::filter(tipo == "bio", subs_urna == 0) |>
   dplyr::mutate(atraso =
-                  ifelse(tempo_atraso >= lubridate::hms("01:00:00"), 1, 0),
-                tempo_total = tempo_atraso + 9)
+                  ifelse(tempo_atraso >= lubridate::hms("01:00:00"), 1, 0)
+                )
 
 # estatistica descritiva --------------------------------------------------
 
@@ -60,7 +60,7 @@ alpha <- coef(modelo_lm)[[1]]
 # extrai o valor do beta
 beta <- coef(modelo_lm)[[2]]
 
-# calcula o parametro de calibracao
+# calcula o parametro de calibracao (81s)
 x0 <- (81 - alpha) / beta
 
 # calcula a variancia estimada do parametro de calibracao
@@ -78,32 +78,71 @@ boot_func <- function(data, indices) {
 }
 
 # cria amostras bootstrap
-boot_results <- boot::boot(data = dados, statistic = boot_func, R = 200)
+boot_results <- boot::boot(data = dados, statistic = boot_func, R = 1000)
 
 # estima os intervalos de confianca bootstrap
-boot::boot.ci(boot_results)
+icbooot <- boot::boot.ci(boot_results)
 
 # curva ROC ---------------------------------------------------------------
 
-# ajusta o modelo de regressao logistica
-modelo_logistico <- glm(atraso ~ QT_APTOS, data = dados, family = "binomial")
+#criação da curva com o pacote pROC
 
-# inclui termos uteis no banco de dados
-broom::augment(modelo_logistico, dados) |>
-  View()
+curva_roc <- dados |>
+  pROC::roc(atraso, qt_aptos)
 
-# constroi a curva roc para avaliar o modelo
-ggplot2::ggplot(dados_teste, ggplot2::aes(d = atraso, m = .hat)) +
-  plotROC::geom_roc(labelround = 4)
+
+
+
+#Ponto de corte que maximiza a sensibilidade e especificidade
+ponto_corte_best <- pROC::coords(curva_roc, "best",
+                                 ret = c("threshold",
+                                         "specificity",
+                                         "sensitivity"))
+
+
+#Gráfico
+pROC::ggroc(curva_roc) +
+  ggplot2::labs(
+    title = "Curva ROC",
+    x = "Especificidade",
+    y = "Sensibilidade"
+  ) +
+  ggplot2::geom_abline(slope = 1, intercept = 1, linetype = "dashed") +
+  ggplot2::geom_point(data = ponto_corte_best,
+                      ggplot2::aes(
+                        x = specificity,
+                        y = sensitivity
+                      ),
+                      color = "red",
+                      size = 3
+  ) +
+  ggplot2::geom_label( data = ponto_corte_best,
+                      ggplot2::aes(
+                        x = specificity,
+                        y = sensitivity,
+                        label = paste0("Corte: ", ceiling(threshold))
+                      ),
+                      nudge_x = 0.1,
+                      nudge_y = -0.1,
+                      color = "red"
+  ) +
+  ggplot2::theme_minimal()
+
+#Intervalo de confiança do ponto de corte "best"
+ic_ponto_corte <- pROC::ci.coords(curva_roc, "best")
 
 # arvore de decisao -------------------------------------------------------
 
-# estima a arvore de decisao
+# estima a arvore de decisao baseado no tempo de atraso considerando
 modelo_arvore <- rpart::rpart(
-  tempo_atraso ~ qt_aptos,
+  atraso ~ qt_aptos,
   data = dados,
   control = rpart::rpart.control(maxdepth = 1)
 )
+
+
+# constroi o grafico da arvore de forma mais amigavel
+rpart.plot::rpart.plot(modelo_arvore)
 
 # apresenta a estrutura da arvore
 plot(modelo_arvore)
@@ -114,6 +153,5 @@ roc <- pROC::roc(dados$atraso, predict(modelo_arvore))
 # constroi o grafico da curva ROC
 plot(roc)
 
-# constroi o grafico da arvore de forma mais amigavel
-rpart.plot::rpart.plot(modelo_arvore)
+
 
